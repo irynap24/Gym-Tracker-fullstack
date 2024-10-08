15786c76027c4d772adfe1234f8609e7f9681338
irynap24 <irynapih@gmail.com>
2024-10-08 17:01:24 -0400

    created workout history page

--- frontend/src/pages/Workouts.jsx
+++ frontend/src/pages/Workouts.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import WorkoutForm from "../components/WorkoutForm";
+
 function Workouts() {
-  return <div> Workouts Database </div>;
+  const [workouts, setWorkouts] = useState([]);
+  const addWorkout = (newWorkout) => {
+    setWorkouts([...workouts, newWorkout]);
+  };
+
+  return (
+    <div>
+      <h2>Track Your Workouts</h2>
+      <WorkoutForm addWorkout={addWorkout} />
+
+      <h3>Your Logged Workouts</h3>
+      <ul>
+        {workouts.map((workout, index) => (
+          <li key={index}>
+            <strong>{workout.type}</strong> - {workout.duration} mins on{" "}
+            {workout.date}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default Workouts;
