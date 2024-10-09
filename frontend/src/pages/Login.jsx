cba38d60918a00a6c579878ad40eea339171e0b0
irynap24 <irynapih@gmail.com>
2024-10-08 21:02:09 -0400

    created login page and incorporated redirect to register page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,46 @@
+import React from "react";
+
+import "./Login.css"; //
+import { useNavigate } from "react-router-dom";
+
 function Login() {
-  return <div>Log in page</div>;
+  const navigate = useNavigate();
+
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+  return (
+    <div className="wrapper">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Username" required />
+          <i className="bx bxs-user"></i>
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+          <i className="bx bxs-lock-alt"></i>
+        </div>
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Remember Me
+          </label>
+          <a href="#">Forgot Password</a>
+        </div>
+        <button type="submit" className="btn">
+          Login
+        </button>
+        <div className="register-link">
+          <p>
+            Don't have an account?{" "}
+            <button onClick={handleRegisterClick} className="register-btn">
+              Register
+            </button>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
