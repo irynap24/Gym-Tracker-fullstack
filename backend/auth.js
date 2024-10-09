7dd7bd4d939f3ba1ceecf9115ebcb956749d247c
irynap24 <irynapih@gmail.com>
2024-10-09 11:54:00 -0400

    added auth logic for registering and logging in

---
+++ backend/auth.js
@@ -0,0 +1,25 @@
+import { auth } from './firebaseConfig.js'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+
+export const registerUser = async (email, password) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+        return userCredential.user
+    }
+    catch (error) {
+        console.error("Error ergistering user:", error)
+        throw error;
+    }
+
+}
+
+export const loginUser = async (email, password) => {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password)
+        return userCredential.user
+    } catch (error) {
+        console.error("Error loggin in", error)
+        throw error;
+    }
+
+}
