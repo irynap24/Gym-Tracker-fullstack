e1d50c513c954d00bcf61da4f4c74f29a7fb9f8b
irynap24 <irynapih@gmail.com>
2024-10-16 17:08:16 -0400

    configuring .env

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
@@ -12,6 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
 
-
+export { auth };
